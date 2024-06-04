1e4f9c1b43a3e39346ac2c2427b47bbcc4b10d07
cristianbyte <charlsmaritz@gmail.com>
2024-06-04 06:55:00 -0500

    create: user entity created

---
+++ src/main/java/com/cristianbyte/survey/domain/entities/User.java
@@ -0,0 +1,30 @@
+package com.cristianbyte.survey.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(nullable = false, length = 100)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private boolean active;
+}
