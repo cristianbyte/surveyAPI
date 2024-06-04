155ef99429bd33c361e8652e1bb8f19c1e3b24b5
cristianbyte <charlsmaritz@gmail.com>
2024-06-04 07:02:10 -0500

    create: all entities

--- src/main/java/com/cristianbyte/survey/domain/entities/OptionQs.java
+++ src/main/java/com/cristianbyte/survey/domain/entities/OptionQs.java
@@ -1,45 +1,35 @@
 package com.cristianbyte.survey.domain.entities;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-@Entity
 @Getter
 @Setter
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class OptionQs {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
     
-    @Column(nullable = false, length = 100)
-    private String name;
-
-    @Column(nullable = false, length = 100)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "id")
+    private Question question;
+    
     @Column(nullable = false)
     private boolean active;
-
-    // units
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
-    @OneToMany(mappedBy = "creator")
-    private List<Survey> surveys;
 }
--- src/main/java/com/cristianbyte/survey/domain/entities/Question.java
+++ src/main/java/com/cristianbyte/survey/domain/entities/Question.java
@@ -1,12 +1,16 @@
 package com.cristianbyte.survey.domain.entities;
 
+
 import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -15,31 +19,31 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @Getter
 @Setter
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
-    @Column(nullable = false, length = 100)
-    private String name;
-
-    @Column(nullable = false, length = 100)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
+    
+    @Column(nullable = false, length = 50)
+    private String type;
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "id")
+    private Survey survey;
+    
     @Column(nullable = false)
     private boolean active;
 
-    // units
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @OneToMany(mappedBy = "creator")
-    private List<Survey> surveys;
+    @OneToMany(mappedBy = "question")
+    private List<OptionQs> options;
 }
