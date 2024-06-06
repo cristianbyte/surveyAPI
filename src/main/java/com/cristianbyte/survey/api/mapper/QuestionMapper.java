ffe54e254ba0e55cb57a2c29683f027fa62e4c30
cristianbyte <charlsmaritz@gmail.com>
2024-06-06 16:08:05 -0500

    hotfix: feature/task-dev-05 mappin options list

--- src/main/java/com/cristianbyte/survey/api/mapper/QuestionMapper.java
+++ src/main/java/com/cristianbyte/survey/api/mapper/QuestionMapper.java
@@ -9,18 +9,16 @@ import com.cristianbyte.survey.api.dto.request.QuestionRequest;
 import com.cristianbyte.survey.api.dto.response.QuestionResponse;
 import com.cristianbyte.survey.domain.entities.Question;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", uses = {OptionMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface QuestionMapper {
     @Mapping(target = "survey.id", source = "survey")
-    @Mapping(target = "options", ignore=true)
+    @Mapping(target = "options", ignore = true)
     Question questionRequestToQuestion(QuestionRequest request);
-    
-    @Mapping(target = "options", ignore=true)
+
     @Mapping(target = "survey", source = "survey.id")
     QuestionResponse questionToQuestionResponse(Question question);
-    
-    @Mapping(target = "survey", ignore=true)
-    @Mapping(target = "options", ignore=true)
-    //@Mapping(target = "options", ignore=true)
+
+    @Mapping(target = "survey", ignore = true)
+    @Mapping(target = "options", ignore = true)
     void updateFromQuestionRequest(QuestionRequest request, @MappingTarget Question question);
 }
